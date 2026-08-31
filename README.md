c085d9101ed13308568b1f5dc1a0024e73e0c1a8
aelobdog <dev.godboleashwin@gmail.com>
2026-08-31 05:59:13 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AOCLFFTZ.jl
+
+An AbstractFFT compatible implementation for AMD's [AOCL FFTZ](https://github.com/amd/aocl-fftz) library.
---
+++ src/AOCLFFTZ.jl
@@ -0,0 +1,8 @@
+# Copyright (c) 2026 Ashwin Godbole
+# SPDX-License-Identifier: MIT
+
+module AOCLFFTZ
+
+greet() = print("Hello World!")
+
+end # module AOCLFFTZ
